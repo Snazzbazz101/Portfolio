6c3462d0f90e97b3105fa2c58a85a0d3aeeb1822
Daniel <181678706+Snazzbazz101@users.noreply.github.com>
2025-03-07 14:37:09 -0800

    added ui navbar and bento grid;

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -11,8 +11,8 @@ const Hero = () => {
         <div>
             {/*spotlight*/}
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill="white"/>
-            <Spotlight className='-top-10 -left-full h-[80vh] w-[50vw]' fill="purple"/>
-            <Spotlight className='-top-28 -left-80 h-[80vh] w-[50vw]' fill="blue"/>
+            <Spotlight className='-top-10 -left-full h-[80vh] w-[50vw] ' fill= "purple"/>
+            <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill="blue"/>
         </div>
         {/*grid container*/}
         <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.04] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0"/>
@@ -34,7 +34,10 @@ const Hero = () => {
                     A Computer Science Major at Simon Fraser University
                 </p>
                 <a href="#about">
-                <ShimmerButton title="View My Work" icon={<FaLocationArrow />} position="right" otherClasses=''/>
+                <ShimmerButton title="View My Work" 
+                icon={<FaLocationArrow />} 
+                position="right" 
+                otherClasses=''/>
                 </a>
 
             </div>
--- portfolio/components/ui/shimmerButton.tsx
+++ portfolio/components/ui/shimmerButton.tsx
@@ -17,7 +17,7 @@ const ShimmerButton= ({
 }) => {
   return (
         // Button code
-        <button className={`gap-2 md:w-50 md:mt-10 w-full rounded-md inline-flex h-12 animate-shimmer items-center justify-center border border-slate-400 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 font-medium text-slate-400 transition-colors${otherClasses}`}>
+        <button className={`gap-2 md:w-50 md:mt-10 w-full rounded-md inline-flex h-12 animate-shimmer items-center justify-center border border-slate-400 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 font-medium text-slate-400 transition-colors ${otherClasses}`}>
           {position == 'left' && icon}
           {title}
           {position == 'right' && icon}
